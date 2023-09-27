264faaa6aa81a10b9ea364640282736b90003f09
OlgaKashkur <112951675+OlgaKashkur@users.noreply.github.com>
2023-09-27 18:16:42 +0300

    feat: add search

--- js30#2.2-image-galery/main.js
+++ js30#2.2-image-galery/main.js
@@ -1,7 +1,8 @@
-const API_KEY = "8c8e1a50-6322-4135-8875-5d40a5420d86";
+
 const API_URL = "https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo";
-const API_URL_SEARCH =
-  "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
+const API_URL_SEARCH ="https://api.unsplash.com/search/photos?query=";
+
+const api_end_url = "&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo"
 
 getImages(API_URL);
 
@@ -9,15 +10,14 @@ async function getImages(url) {
   const resp = await fetch(url);
   const respData = await resp.json();
   showImages(respData);
-
 }
 
 function showImages(data) {
   const imagesEl = document.querySelector(".images");
 
-  // Очищаем предыдущие фильмы
-  /*document.querySelector(".images").innerHTML = "";*/
- //results  это элементы массива date
+  // Очищаем предыдущие img после каждого запроса
+  document.querySelector(".images").innerHTML = "";
+ //results  это элементы массива 
   data.results.forEach((image) => {
     const imagEl = document.createElement("div");
     imagEl.classList.add("image");
@@ -30,16 +30,16 @@ function showImages(data) {
   });
 }
 
-/*const form = document.querySelector("form");
-const search = document.querySelector(".header__search");
+const form = document.querySelector("form");
+const searchWorld = document.querySelector(".search");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+  e.preventDefault(); // что бы не было перезагрузки страницы при нажатии на поиск
 
-  const apiSearchUrl = `${API_URL_SEARCH}${search.value}`;
-  if (search.value) {
-    getMovies(apiSearchUrl);
+  const apiSearch = `${API_URL_SEARCH}${searchWorld.value}${api_end_url}`;
+  if (searchWorld.value) {
+    getImages(apiSearch);
 
-    search.value = "";
+    searchWorld.value = "";// Очищаем предыдущий запрос
   }
-});*/
+});
